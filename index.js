ea58841ab243a7b94399a332f345258977eadf5c
foryou929 <zxcvwert3457@gmail.com>
2024-07-28 02:27:27 +0900

    feat: add-upload-api

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
+app.get('/api/upload', (req, res) => {
+    
+})
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
