5451d7e304c37348500c151ffbd17254e3b7bfa7
foryou929 <zxcvwert3457@gmail.com>
2024-08-02 21:44:01 +0900

    feat: add-upload

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 
-const port = process.env.PORT;
 const app = express();
 
-app.get('/api/upload', (req, res) => {
-    
+app.use(cors());
+app.use(bodyParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+const router = express.Router();
+
+const upload = multer({ storage });
+
+router.post(
+    '/process',
+    upload.fields([
+        { name: 'audio', maxCount: 1 },
+        { name: 'video', maxCount: 1 },
+    ]),
+    (req, res) => {
+        res.send('File uploaded');
+    }
+)
+
+app.use('/api', router);
+
+app.all('*', (req, res) => {
+    res.send('Not implemented');
 })
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
