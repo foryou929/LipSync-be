ce59b3fb2872093515d4b45921d5e3d3bf6a2c4c
foryou929 <zxcvwert3457@gmail.com>
2024-08-03 16:31:22 +0000

    feat: handle-error-exception

--- index.js
+++ index.js
@@ -35,21 +35,26 @@ router.post(
         { name: 'video', maxCount: 1 },
     ]),
     (req, res) => {
-        const audioFilePath = req.files.audio[0].path;
-        const videoFilePath = req.files.video[0].path;
-        const audioFileInfo = path.parse(audioFilePath);
-        const audioFileNameWithoutExt = audioFileInfo.name;
-        const videoFileInfo = path.parse(videoFilePath);
-        const videoFileNameWithoutExt = videoFileInfo.name;
-        exec(`~/LipSync-core/env/bin/python run.py -video_file ~/LipSync-be/${videoFilePath} -vocal_file ~/LipSync-be/${audioFilePath} -output_file output/output.mp4`, { cwd: '../LipSync-core' }, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return res.status(500).json({ error: 'Error running process' });
-            }
-            console.log(`stdout: ${stdout}`);
-            console.error(`stderr: ${stderr}`);
-            res.json({ message: `Process output: ${stdout}`, url: `${videoFileNameWithoutExt}_${audioFileNameWithoutExt}_LipSync.mp4` });
-        });
+        try {
+            const audioFilePath = req.files.audio[0].path;
+            const videoFilePath = req.files.video[0].path;
+            const audioFileInfo = path.parse(audioFilePath);
+            const audioFileNameWithoutExt = audioFileInfo.name;
+            const videoFileInfo = path.parse(videoFilePath);
+            const videoFileNameWithoutExt = videoFileInfo.name;
+            exec(`~/LipSync-core/env/bin/python run.py -video_file ~/LipSync-be/${videoFilePath} -vocal_file ~/LipSync-be/${audioFilePath} -output_file output/output.mp4`, { cwd: '../LipSync-core' }, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return res.status(500).json({ error: 'Error running process' });
+                }
+                console.log(`stdout: ${stdout}`);
+                console.error(`stderr: ${stderr}`);
+                res.json({ message: `Process output: ${stdout}`, url: `${videoFileNameWithoutExt}_${audioFileNameWithoutExt}_LipSync.mp4` });
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).json({ error: err.message });
+        }
     }
 )
 
